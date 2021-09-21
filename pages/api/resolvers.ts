80ad05538a7bc18175b7bb00de6de909c891a601
Brent Abruzese <brentabruzese@Brents-MBP.fios-router.home>
2021-09-21 11:21:04 -0400

    var name changed

--- pages/api/resolvers.ts
+++ pages/api/resolvers.ts
@@ -16,12 +16,12 @@ export const resolvers = {
   Mutation: {
     login: (_: undefined, args: any, ctx: undefined) => {
       const { username, password } = args.input;
-      const foundUser = users.filter(
+      const validUser = users.filter(
         (user: UserAttributes) =>
           user.username === username && user.password === password
       );
 
-      if (foundUser.length > 0) {
+      if (validUser.length > 0) {
         return { id: "1", token: "sdfj4ljsdjkf4jkisdiofmsdfiok4" };
       } else {
         throw new Error("Login was incorrect or the user doesnt exist!");
